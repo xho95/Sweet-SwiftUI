c39bfb3b741f7ff289dab283f326b3a988f339b8
Kim, Min Ho <xho1995@gmail.com>
2022-06-12 16:48:17 +0900

    add a new project to practice a SwiftUI

---
+++ FruitMartMe/FruitMartMe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FruitMartMe
+//
+//  Created by Min Ho Kim on 2022/06/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FruitMartMe/FruitMartMe/FruitMartMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitMartMeApp.swift
+//  FruitMartMe
+//
+//  Created by Min Ho Kim on 2022/06/12.
+//
+
+import SwiftUI
+
+@main
+struct FruitMartMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
