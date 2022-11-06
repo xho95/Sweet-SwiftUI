00b1f5557df515c092c5f877ce2775b6aa4ee67a
Kim, Min Ho <xho1995@gmail.com>
2022-11-06 17:18:28 +0900

    Initialize the chapter 6 with ViewModifier

---
+++ 06. Customizing/CustomModifier/CustomModifier/CustomModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomModifierApp.swift
+//  CustomModifier
+//
+//  Created by Min Ho Kim on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct CustomModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
